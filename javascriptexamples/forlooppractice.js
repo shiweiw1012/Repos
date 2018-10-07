bab6c48385e8d2af0e19bbb70c4f7e615551807e
Shiwei Wang <shiweiw@g.clemson.edu>
2018-10-07 19:46:50 -0400

    code practice

---
+++ javascriptexamples/forlooppractice.js
@@ -0,0 +1,7 @@
+var n = 25;
+
+var sum = 0;
+
+for (var i = 0; i < n; i++) {
+	sum = sum + i;
+}
---
+++ javascriptexamples/ifelsepractice.js
@@ -0,0 +1,11 @@
+var n = 25;
+var result = 0;
+
+if (n % 2 == 0) {
+	console.log("Even");
+	result+10
+	} else {
+	console.log("Odd");
+	result=result-n
+	}
+	
