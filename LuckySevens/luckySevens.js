1120894149b2b1697024c2b61fee06f7e7a60d7c
Shiwei Wang <shiweiw@g.clemson.edu>
2018-10-11 12:51:51 -0400

    if statement for win and lose

--- LuckySevens/luckySevens.js
+++ LuckySevens/luckySevens.js
@@ -7,16 +7,33 @@ function luckySevens() {
   var gameMoney = startingBet;
   var moneyArray = [startingBet];
 
-if (startingBet <= 0) {
-    alert("Starting bet must be greater than 0");
-	document.forms["luckySevens"]["startingBet"].value = "";
-    return false;
-}
-while (gameMoney > 0) {
-    totalRoll++;
-
-	var dice1 = Math.floor(Math.random() * 6) + 1;
-    var dice2 = Math.floor(Math.random() * 6) + 1;
-    var sumDice = dice1 + dice2;
+	if (startingBet <= 0) {
+		alert("Starting bet must be greater than 0");
+		document.forms["luckySevens"]["startingBet"].value = "";
+		return false;
+	}
 	
+	while (gameMoney > 0) {
+		totalRoll++;
+
+		var dice1 = Math.floor(Math.random() * 6) + 1;
+		var dice2 = Math.floor(Math.random() * 6) + 1;
+		var sumDice = dice1 + dice2;
+	
+	if (sumDice == 7) {
+		gameMoney = gameMoney + 4; // win
+		winCounter++; // how many times you win
+		var oldMoney = moneyArray[moneyArray.length - 1]; // shows current amount of money user has
+		var newMoney = oldMoney + 4; // updates amount of money user has
+		moneyArray.push(newMoney); // add updated amount to end of array
+    }
+
+    else {
+		gameMoney = gameMoney - 1; //lose
+		loseCounter++; // how many times you lose
+		var oldMoney = moneyArray[moneyArray.length - 1]; // shows current amount of money user has
+		var newMoney = oldMoney - 1; // updates amount of money user has
+		moneyArray.push(newMoney); // add updated amount to end of array
+    }
+
 }
