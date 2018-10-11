34d1f1554209999a2d8668926571e6816914dea0
Shiwei Wang <shiweiw@g.clemson.edu>
2018-10-11 09:20:36 -0400

    update on function

--- LuckySevens/luckySevens.js
+++ LuckySevens/luckySevens.js
@@ -7,8 +7,16 @@ function luckySevens() {
   var gameMoney = startingBet;
   var moneyArray = [startingBet];
 
-  if (startingBet <= 0) {
+if (startingBet <= 0) {
     alert("Starting bet must be greater than 0");
 	document.forms["luckySevens"]["startingBet"].value = "";
     return false;
+}
+while (gameMoney > 0) {
+    totalRoll++;
+
+	var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+    var sumDice = dice1 + dice2;
+	
 }
